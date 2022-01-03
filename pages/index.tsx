f2147b6a335972a596464fa446806495e3d055ac
lbh0731 <lbh0731@midasit.com>
2022-01-04 00:31:30 +0900

    feature:next 추가

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import {FC} from "react";
+import styled from "styled-components";
+
+const Frame = styled.div`
+`
+
+const StartPage: FC  = () =>{
+  return (
+    <Frame>
+      aaa
+    </Frame>
+  )
+}
+
+export default StartPage
