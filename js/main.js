a7e4a645395b8e1bbe25d9d35ee3109c9a4d5f3e
CookieMon-HH <lhh10133@gmail.com>
2022-01-11 23:53:12 +0900

    2-5 활성시킬 씬 적용하기
    2-5 활성시킬 씬 적용하기

--- js/main.js
+++ js/main.js
@@ -52,12 +52,25 @@
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
             //html파일 section에 style="height: ##px;" 추가하면 해당 section의 높이가 됨 
             //js파일에서 query selector를 통해 section을 가져오고 style method를 통해 수정 가능
+        }
+
+        //setLayout 할 때 마다 currentScene도 설정해줌
+        yOffset = window.pageYOffset;
+        
+        let totalScrollHeight = 0;
+        for(let i =0; i < sceneInfo.length; i++) {
+            totalScrollHeight += sceneInfo[i].scrollHeight;
+            if (totalScrollHeight >= yOffset) {
+                currentScene = i;
+                break;
+            }
+
         } 
     }
 
     function scrollLoop() {
         //현재 scene을 0으로 시작하고 스크롤할 때 마다 scene 변경 조건을 확인해서 증가시키거나 감소시키는듯 
-        //아래서 새로고침해도 스크롤 하면 조건에 들어오니까 바로 찾을 수 있는듯
+        //아래서 새로고침해도 스크롤 하면 조건에 들어오니까 바로 찾을 수 있는듯 (setLayout에 세팅 없었을때)
         //현재 화면의 상단과 해당 section이 만나는 경우가 전환시점
         prevScrollHeight = 0;
         for(let i = 0; i<currentScene; i++) {
@@ -68,24 +81,23 @@
             currentScene ++;
         }
         //(아래 방향으로 스크롤)현재 y 위치가 이전 scene들의 높이+지금 secen의 높이보다 커지면 증가  
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
         if(yOffset < prevScrollHeight){
             if (currentScene==0) return; //브라우저 바운스 효과로 인해 yoffset이 -가 되는 경우가 있어 scene이 음수가 되는것을 방지
-            currentScene --;
+            currentScene --; 
         }
         //(위 방향으로 스크롤) 현재 y 위치가 이전 scene들의 높이보다 작아지면 감소
-
-        console.log(currentScene);
-
+        document.body.setAttribute('id',`show-scene-${currentScene}`);
     }
 
-
-    window.addEventListener('resize', setLayout); 
     window.addEventListener('scroll', () => {
         yOffset = window.pageYOffset;
         scrollLoop()
     });
-
-    setLayout();
+    
+    window.addEventListener('load', setLayout);
+    window.addEventListener('resize', setLayout);
+    
 })();
 // 스크롤 영역을 배열로 지정
 // 함수를 선언하고 바로 호출
