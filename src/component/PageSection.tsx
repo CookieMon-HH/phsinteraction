5775dd3231aaf4a66478432d998a7f86dcfe206a
lbh0731 <lbh0731@midasit.com>
2022-01-11 16:21:55 +0900

    chore: 미반영 변경사항 반영

--- src/component/PageSection.tsx
+++ src/component/PageSection.tsx
@@ -15,7 +15,7 @@ const Frame = styled.section`
   }
 `;
 
-const ElementMainMessageDiv = styled.div`
+const ElementDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -75,7 +75,7 @@ const DescriptionP = styled.p`
   
 `;
 
-const SticyElemDescriptionMessageDiv = styled.div`
+const DescriptionDiv = styled.div`
   ${fonts.BOLD}
   width: 50%;
 
@@ -121,38 +121,38 @@ const PageSection: FC = (() => {
   return (
     <Frame>
       <h1>AirMug Pro</h1>
-      <ElementMainMessageDiv>
+      <ElementDiv>
         <p>온전히 빠져들게 하는<br/>최고급 세라믹</p>
-      </ElementMainMessageDiv>
-      <ElementMainMessageDiv>
+      </ElementDiv>
+      <ElementDiv>
         <p>주변 맛을 느끼게 해주는<br/>주변 맛 허용 모드</p>
-      </ElementMainMessageDiv>
-      <ElementMainMessageDiv>
+      </ElementDiv>
+      <ElementDiv>
         <p>온종일 편안한<br/>맞춤형 손잡이</p>
-      </ElementMainMessageDiv>
-      <ElementMainMessageDiv>
+      </ElementDiv>
+      <ElementDiv>
         <p>새롭게 입가를<br/>찾아온 매혹</p>
-      </ElementMainMessageDiv>
+      </ElementDiv>
       <DescriptionP>
         <strong>보통 스크롤 영역</strong>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit optio impedit qui quam, error quidem quis vero!
         Sit eaque doloremque sunt cum? Impedit id sequi cupiditate at accusamus architecto eveniet.
       </DescriptionP>
-      <ElementMainMessageDiv className={'another'}>
+      <ElementDiv className={'another'}>
         <p>
           <small>편안한 촉감</small>
           입과 하나 되다
         </p>
-      </ElementMainMessageDiv>
-      <SticyElemDescriptionMessageDiv>
+      </ElementDiv>
+      <DescriptionDiv>
         <p>
           편안한 목넘김을 완성하는 디테일한 여러 구성 요소들, 우리는 이를 하나하나 새롭게 살피고 재구성하는 과정을 거쳐 새로운 수준의 머그, AirMug Pro를 만들었습니다. 입에 뭔가 댔다는 감각은
           어느새 사라지고 오롯이 당신과 음료만 남게 되죠.
         </p>
-      </SticyElemDescriptionMessageDiv>
-      <SticyElemDescriptionMessageDiv>
+      </DescriptionDiv>
+      <DescriptionDiv>
         디자인 앤 퀄리티 오브 스웨덴,<br/>메이드 인 차이나
-      </SticyElemDescriptionMessageDiv>
+      </DescriptionDiv>
       <PinDiv/>
       <MidMessageP>
         <strong>Retina 머그</strong><br/>
